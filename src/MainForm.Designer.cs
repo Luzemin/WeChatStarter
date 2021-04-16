f393f117f9f4338f6912c2db1394202d625d3a58
unknown <592640424@qq.com>
2021-04-16 10:11:18 +0800

    init

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WechatMore
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.countBox = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // countBox
+            // 
+            this.countBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.countBox.Font = new System.Drawing.Font("Georgia", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countBox.ForeColor = System.Drawing.Color.Green;
+            this.countBox.Location = new System.Drawing.Point(95, 20);
+            this.countBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(175, 152);
+            this.countBox.TabIndex = 1;
+            this.countBox.TabStop = false;
+            this.countBox.Text = "2";
+            this.countBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.Green;
+            this.startBtn.FlatAppearance.BorderSize = 0;
+            this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startBtn.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.ForeColor = System.Drawing.Color.White;
+            this.startBtn.Location = new System.Drawing.Point(117, 218);
+            this.startBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(129, 45);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(122, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "您的电脑未安装微信";
+            this.label1.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(377, 309);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.countBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "微信多启动器";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox countBox;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ src/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WechatMore
+{
+    public partial class MainForm : Form
+    {
+        private string wechatPath = string.Empty;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            string count = countBox.Text;
+            if (string.IsNullOrEmpty(wechatPath) || string.IsNullOrEmpty(count) || !int.TryParse(count, out int openCount))
+            {
+                return;
+            }
+            if (openCount < 1 || openCount > 100)
+            {
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("-command \"");
+            for (int i = 0; i < openCount; i++)
+            {
+                sb.Append("&'" + wechatPath + "';");
+            }
+            sb.Append("\"");
+            string cmd = sb.ToString();
+
+            Process.Start(new ProcessStartInfo("powershell.exe")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                Arguments = cmd
+            });
+
+            Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //自动查找微信.exe
+            //盲猜
+            var drivers = DriveInfo.GetDrives().AsEnumerable();
+            string programFiles = Environment.Is64BitOperatingSystem ? "Program Files (x86)" : "Program Files";
+            foreach (var driver in drivers)
+            {
+                var path = driver.Name + programFiles + @"\Tencent\WeChat\WeChat.exe";
+                if (File.Exists(path))
+                {
+                    wechatPath = path;
+                    return;
+                }
+            }
+
+            //从卸载列表中找
+            string regPath = Environment.Is64BitOperatingSystem ? @"HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" :
+                @"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\";
+            string cmd = "(Get-ItemProperty " + regPath + "* | where{$_.PSChildName -eq 'WeChat'}).InstallLocation";
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("powershell.exe")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                Arguments = "-command " + cmd,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+            process.Start();
+            string outputResult = process.StandardOutput.ReadToEnd();
+            string errorResult = process.StandardError.ReadToEnd();
+            if (!string.IsNullOrEmpty(outputResult))
+            {
+                wechatPath = outputResult.Trim() + "\\Wechat.exe";
+            }
+            else
+            {
+                label1.Visible = true;
+                startBtn.Enabled = false;
+            }
+        }
+
+        private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                Start();
+            }
+            else if (e.KeyChar == (char)Keys.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
